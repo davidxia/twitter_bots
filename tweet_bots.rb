bfe458110cc4d759bdda6369fbee2df6829e0a6d
David Xia <david@davidxia.com>
2012-02-07 13:04:42 -0500

    Added new bots, use dbi gem

--- tweet_bots.rb
+++ tweet_bots.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-$LOAD_PATH << './'
+#$LOAD_PATH << '/home/david/twitter_bots/'
 
-require 'twitterbot.rb'
+require '/home/david/twitter_bots/twitterbot.rb'
 
 mcnulty = TwitterBot.new('jamesmcnulty')
 mcnulty.tweet
@@ -18,3 +18,36 @@ bauer.tweet
 bauer.reply
 bauer.close
 
+sookie = TwitterBot.new('sookie_stakhaus')
+sookie.tweet
+sookie.reply
+sookie.close
+
+walter = TwitterBot.new('walterwhite_')
+walter.tweet
+walter.reply
+walter.close
+
+house = TwitterBot.new('doctor_ghouse')
+house.tweet
+house.reply
+house.close
+
+stark = TwitterBot.new('ned_stark_')
+stark.tweet
+stark.reply
+stark.close
+
+fred = TwitterBot.new('portlandiafred')
+fred.converse_with('portlandiacarie')
+fred.tweet
+fred.reply
+fred.close
+
+carrie = TwitterBot.new('portlandiacarie')
+carrie.converse_with('portlandiafred')
+carrie.tweet
+carrie.reply
+carrie.close
+
+puts 'done'
